ceaf9aab8925afaf39adb17987000f294bca91bc
LuanM280 <luanlima01388@gmail.com>
2026-04-02 13:48:23 -0300

    test 2

--- app/conexao/bancos.js
+++ app/conexao/bancos.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js'
 const supabase = createClient('https://ekdskhpbgorgflhhehfp.supabase.co', 'sb_publishable_IXnnnkyVkAxmOe4AhwF6VA_F3RzJrnJ')
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap-icons/font/bootstrap-icons.css';
 
 export default supabase
