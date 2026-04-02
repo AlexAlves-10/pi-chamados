9b391dfb6db369d06ae2248d153fefba7d970260
LuanM280 <luanlima01388@gmail.com>
2026-04-01 21:56:43 -0300

    Update page.js

--- app/login/page.js
+++ app/login/page.js
@@ -9,12 +9,8 @@ function Login() {
 
     if(typeof window === "undefined") return null
 
-    const logado = localStorage.getItem("logado")
     const admin = localStorage.getItem('administrador')
-    const id = localStorage.getItem("id_usuario")
-
-    const [autenticado, alteraAutenticado] = useState(false)
-    const [autadmin, alteraAutadmin] = useState(null)
+    const id_usuario = localStorage.getItem("id_usuario")
 
     const [email, alteraEmail] = useState("")
     const [senha, alteraSenha] = useState("")
@@ -44,8 +40,6 @@ function Login() {
             return
         }
 
-        alteraAutadmin(usuario.administrador)
-
         localStorage.setItem('id_usuario', data.user.id)
         localStorage.setItem('administrador', usuario.administrador)
 
@@ -56,23 +50,9 @@ function Login() {
     }
     function desconectar() {
         alert("Desconectado com sucesso!")
-        localStorage.removeItem("logado")
-        alteraAutenticado(false)
+        localStorage.removeItem("id_usuario")
+        localStorage.removeItem("administrador")
     }
-
-    useEffect(() => {
-        
-
-        if (id){
-            alteraAutenticado(true)
-            alteraAutadmin(admin === "true")
-        }
-
-        if (logado == "true") {
-            alteraAutenticado(true)
-        }
-
-    }, [])
 
 
     return (
@@ -84,7 +64,7 @@ function Login() {
             alignItems: "center",
         }}>
             {
-                autenticado == false ?
+                id_usuario == null ?
                     <div className="card sadow" style={{
                         width: "350px",
                         borderRadius: "15px",
@@ -120,17 +100,6 @@ function Login() {
                         }} onClick={desconectar} type='button' className="btn btn-outline-danger" > Sair da conta </button>
                     </div>
 
-            }
-
-            {
-                autadmin == true ?
-                    <div>
-
-                    </div>
-                    :
-                    <div>
-
-                    </div>
             }
         </div>
     );
