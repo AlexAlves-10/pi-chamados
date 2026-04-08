0ed492f53628aa5d96fd121f600ff93a99d8ddf1
Marcos Kenji Ban Ribeiro <marcoskenji07@gmail.com>
2026-04-08 20:26:37 -0300

    Update page.js

--- app/page.js
+++ app/page.js
@@ -107,12 +107,6 @@ export default function Home() {
         Gerencie solicitações, acompanhe atendimentos e organize demandas de forma simples e eficiente.
       </p>
 
-      <div className="mt-4">
-        <a href="/login" className="btn btn-outline-secondary">
-          Acessar Sistema
-        </a>
-      </div>
-
       <div className="row mt-5">
         <div className="col-md-4">
           <h5>📌 Registro rápido</h5>
